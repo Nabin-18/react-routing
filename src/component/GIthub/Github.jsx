83dc757c7bbf7f8b148e8e565d1091ad127560b6
Nabin khanal <khanalnabin310@gmail.com>
2024-04-30 13:57:59 +0545

    gitfollowers images

--- src/component/GIthub/Github.jsx
+++ src/component/GIthub/Github.jsx
@@ -3,20 +3,57 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 
 function Github() {
-    const [data,setData]=useState([])
-    useEffect(()=>{
-        fetch('https://api.github.com/users/Nabin-18')
-        .then(response =>response.json())
-        .then(data =>{
-            console.log(data)
-            setData(data)
-        })
-    },[])
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/Nabin-18')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setData(data)
+      })
+  }, [])
+  // for followers image
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    fetchData()
+  }, [])
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://api.github.com/users/Nabin-18/followers')
+      const jsonData = await response.json();
+      setPhotos(jsonData);
+
+    } catch (error) {
+      console.log("Error fetching")
+
+    }
+
+  };
+
+
+
   return (
-    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl '>Github Followers:{data.followers}
-    <img className='rounded-3xl' src={data.avatar_url} alt="" width={300}  />
-    
-    </div>
+    <>
+      <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl '>Github Followers:{data.followers}
+        <img className='rounded-3xl' src={data.avatar_url} alt="" width={300} />
+
+
+        <h1>My Github Followers:</h1>
+        <div className='flex px-2 py-2 mx-3 gap-2 flex-wrap'  >
+          {
+            photos.map(item => (
+              <>
+                <img className='rounded-lg' key={item.id} src={item.avatar_url} width={100} />
+                <h1>{item.login}</h1>
+              </>
+            ))
+
+          }
+        </div>
+
+      </div>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import { RouterProvider,createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import About from './component/About/About.jsx'
 import Home from './component/Home/Home.jsx'
@@ -37,21 +37,22 @@ import Github from './component/GIthub/Github.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-   <Route path='/' element={<Layout />}>
-   {/* parents  */}
-   <Route path='' element={<Home />} />
-   <Route path='/about' element={<About />} />
-   <Route path='/contact' element={<Contact />} />
-   <Route path='/user/:userid' element={<User/>} />
-   <Route path='github' element={<Github />} />
-   
-   </Route>
+    <Route path='/' element={<Layout />}>
+      {/* parents  */}
+      <Route path='' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/user/:userid' element={<User />} />
+      <Route path='github' element={<Github />} />
+
+    </Route>
   )
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router} />
-  
+    <RouterProvider router={router} />
+
+
   </React.StrictMode>,
 )
